d40087e063a9eb40b2949d33ef56fa8e317b7d7a
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2021-01-06 23:30:32 -0300

    Criado funçao de adicionar botao remover

--- Lista-de-Tarefas/README.md
+++ Lista-de-Tarefas/README.md
@@ -6,7 +6,7 @@
 - [X] Criar função de Criar Tarefa e mostra pelo menos no console.
 - [X] Criar Elemento que vai fica os valores digitados e mostrar na tela
 - [X] Pegar valor do input quando clicar no ENTER e chama a função
-- [] Criar função para limpa o input (Dica: colocar .focus() => Evento JS)
+- [X] Criar função para limpa o input (Dica: colocar .focus() => Evento JS)
 - [] Criar Função para criar botão para apagar. Usar setAtribute para colocar a class
 - [] Criar evento para apagar a tarefa. (Dica descobrir onde esta sendo clicado para pega o botão, quando descobrir usar o .contains())
 - [] Criar função para salvar as tarefas em um array
--- Lista-de-Tarefas/assets/JS/script.js
+++ Lista-de-Tarefas/assets/JS/script.js
@@ -13,10 +13,12 @@
         
         function pegaValorInput(event){
             event.preventDefault()
-            const valorDigitado = input.value
+            let valorDigitado = input.value
             if(!valorDigitado) return
-            console.log(valorDigitado)
+
             mostraInputTela(valorDigitado)
+            limpaInput()
+            criaBotoes()
         }
 
         function mostraInputTela(digitado){
@@ -24,7 +26,23 @@
             criaNovoInput.disabled = true
             criaNovoInput.value = digitado
             formulario.appendChild(criaNovoInput)
+
+            
         }
+
+        function limpaInput(){
+            input.value = '';
+            input.focus();
+        }
+
+        function criaBotoes(){
+
+            let botaoRemover = document.createElement('button')
+            botaoRemover.setAttribute('class','remover')
+            botaoRemover.innerText = 'Remover';
+            formulario.appendChild(botaoRemover)
+        }
+
 
     }
     escopo()
